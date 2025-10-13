824c435f101665996b3688e3efe7f0de5dbf1d28
Szymon <sz.czudowski@gmail.com>
2025-10-13 22:04:18 +0200

    Config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Root domain for szymoncz.github.io
+  base: "/", // Correct for szymoncz.github.io
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // If using aliases
+    },
+  },
 });
