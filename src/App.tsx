bf76b2003b8df16fa34f27b13fc7aa3b29bf1c00
Szymon <67510803+Szymoncz@users.noreply.github.com>
2025-10-14 11:00:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from ".handly-pjwstk/assets/react.svg";
+import viteLogo from "handly-pjwstk/vite.svg";
 import "./App.css";
 
 function App() {
