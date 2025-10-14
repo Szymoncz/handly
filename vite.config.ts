73ad74e5651a2052c974ad4aaed2b33fb863a4b4
Szymon <67510803+Szymoncz@users.noreply.github.com>
2025-10-14 09:57:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Correct for szymoncz.github.io
+  base: '/handly-pjwstk/', // Match repo name
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // If using aliases
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
