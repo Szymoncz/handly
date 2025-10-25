5f703f3c6125155076c5ae8195d8f0b2530b9e84
Szymon <sz.czudowski@gmail.com>
2025-10-25 10:35:38 +0200

    Vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react/dist';
+// @ts-ignore
 import path from 'path';
 
 export default defineConfig({
