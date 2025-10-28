857b0dab9370ba33bb46101d97fac5a3f3ce6e18
Szymon <sz.czudowski@gmail.com>
2025-10-29 00:28:13 +0100

    New TestComponent added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from './components/TestComponent'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <TestComponent />
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
---
+++ src/components/TestComponent.js
@@ -0,0 +1,7 @@
+import { React } from "react";
+
+export default function TestComponent() {
+    return (
+        <h1>Witaj na stronie Handly!</h1>
+    );
+}
