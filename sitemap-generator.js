7244aa3c9be20975add4473fbb7095064abad0fa
Szymon <sz.czudowski@gmail.com>
2025-11-05 15:33:41 +0100

    Added generate-sitemap script

---
+++ sitemap-generator.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+const BASE_URL = "hhtps://szymoncz.github.io/handly";
+
+const routes = [
+    "/",
+    "/o-nas",
+    "/uslugi",
+    "/kontakt",
+];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes.map((route) => {
+    return ` <url>
+    <loc>${BASE_URL}${route}</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+    </url>`;
+})
+    .join("\n")}
+    </urlset>`;
+
+    fs.writeFileSync("./public/sitemap.xml", sitemap);
+
+console.log("✅ Wygenerowano sitemap.xml w katalogu public/");
