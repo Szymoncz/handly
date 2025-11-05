f5210cc9f674a2eac3aee51543c6ec15c3076a9f
Szymon <sz.czudowski@gmail.com>
2025-11-05 14:57:33 +0100

    Change back from HashRoute to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Menu from './components/Menu'
 import Home from './pages/Home';
 import Onas from './pages/Onas';
 import Uslugi from './pages/Uslugi';
 import Kontakt from './pages/Kontakt';
+
 function App() {
   return (
-      <Router>
+      <>
           <Menu />
           <Routes>
               <Route path="/" element={<Home />} />
@@ -16,7 +17,7 @@ function App() {
               <Route path="/uslugi/" element={<Uslugi />} />
               <Route path="/kontakt/" element={<Kontakt />} />
           </Routes>
-      </Router>
+      </>
   );
 }
 export default App;
