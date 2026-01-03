d180155ce307f73496e7594b4b8a4a880cff3551
Szymon <sz.czudowski@gmail.com>
2026-01-03 18:40:37 +0100

    added basic style

--- src/pages/Logowanie.js
+++ src/pages/Logowanie.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "./logowanie.css";
 
 export default function Logowanie() {
   return (
     <>
-      <p>Zaloguj się!</p>
-      <label for="login">Login</label>
-      <input name="login"></input>
-      <label for="pass">Hasło</label>
-      <input name="pass"></input>
-      <button>Zaloguj</button>
+      <form>
+        <div class="group">
+          <p>Zaloguj się!</p>
+          <label for="login">Login</label>
+          <input name="login" required />
+          <label for="pass">Hasło</label>
+          <input name="pass" required />
+          <input type="submit" value="Zaloguj się do panelu" />
+        </div>
+      </form>
       <p>Nie masz konta? Zarejestruj się!</p>
       <button>Rejestracja</button>
     </>
