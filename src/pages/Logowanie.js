5a486fa35cdf7e0f270e522381fb2d00d3d235ed
Szymon <sz.czudowski@gmail.com>
2026-01-03 18:29:20 +0100

    Login pages fix

--- src/pages/Logowanie.js
+++ src/pages/Logowanie.js
@@ -4,11 +4,13 @@ export default function Logowanie() {
   return (
     <>
       <p>Zaloguj się!</p>
-      <label></label>
-      <input></input>
-      <label></label>
-      <input></input>
-      <button></button>
+      <label for="login">Login</label>
+      <input name="login"></input>
+      <label for="pass">Hasło</label>
+      <input name="pass"></input>
+      <button>Zaloguj</button>
+      <p>Nie masz konta? Zarejestruj się!</p>
+      <button>Rejestracja</button>
     </>
   );
 }
