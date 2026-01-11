9936f1f469e9f7aa6fb0baaafab8e5e9db54b22c
Szymon <sz.czudowski@gmail.com>
2026-01-11 15:54:44 +0100

    Added adding an offer

---
+++ src/components/AddOffer.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+export default function AddOffer({ onOfferCreated }) {
+  async function createOffer(offer) {
+    const response = await fetch("https://apihandly.ddns.net/offers/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + btoa("admin:admin"),
+      },
+      body: JSON.stringify({
+        title: offer.title,
+        description: offer.description,
+        budget: offer.budget,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(JSON.stringify(errorData));
+    }
+
+    return response.json();
+  }
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [budget, setBudget] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const newOffer = await createOffer({
+        title,
+        description,
+        budget,
+      });
+
+      setTitle("");
+      setDescription("");
+      setBudget("");
+
+      onOfferCreated(newOffer);
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+      <h3>Dodaj ofertę</h3>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+
+      <br />
+
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        required
+      />
+
+      <br />
+
+      <input
+        type="number"
+        placeholder="Budget"
+        value={budget}
+        onChange={(e) => setBudget(e.target.value)}
+        required
+      />
+
+      <br />
+
+      <button type="submit">Dodaj ofertę</button>
+    </form>
+  );
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,12 @@
 import Offer from "../components/Offer";
+import AddOffer from "../components/AddOffer";
 
 export default function Dashboard() {
   return (
     <>
       <h1>Dashboard for logged in</h1>
-      <ul>
-        <li>
-          <Offer />
-        </li>
-      </ul>
+      <AddOffer />
+      <Offer />
     </>
   );
 }
