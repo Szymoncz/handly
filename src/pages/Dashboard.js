d5d9758b1b367b5ebf6cf867ab0cfac55f6b4446
Szymon <sz.czudowski@gmail.com>
2026-01-12 10:06:00 +0100

    Fix for double add form

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,9 @@
 import Offer from "../components/Offer";
-import AddOffer from "../components/AddOffer";
 
 export default function Dashboard() {
   return (
     <>
       <h1>Dashboard for logged in</h1>
-      <AddOffer />
       <Offer />
     </>
   );
