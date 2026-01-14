b46fc2d38d7e2b05685426cacce6b3e63a999ab4
Szymon <sz.czudowski@gmail.com>
2026-01-14 23:28:11 +0100

    Structure and div fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "40vh" }}>
-        <h1>Witamy w Handly</h1>
+      <div class="app">
+        <div class="contianer">
+          <h1>Witamy w Handly</h1>
 
-        <Link to="/logowanie">
-          <button>Zaloguj</button>
-        </Link>
+          <Link to="/logowanie">
+            <button>Zaloguj</button>
+          </Link>
 
-        <Link to="/rejestracja">
-          <button style={{ marginLeft: "12px" }}>Rejestracja</button>
-        </Link>
+          <Link to="/rejestracja">
+            <button style={{ marginLeft: "12px" }}>Rejestracja</button>
+          </Link>
+        </div>
       </div>
     </>
   );
