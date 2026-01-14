eb4c2e9df9fea454371dea9233475c45f1663464
Szymon <sz.czudowski@gmail.com>
2026-01-14 12:12:25 +0100

    Fixes for routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
-import Onas from "./pages/Onas";
-import Uslugi from "./pages/Uslugi";
-import Kontakt from "./pages/Kontakt";
 import Footer from "./components/Footer";
 import Logowanie from "./pages/Logowanie";
 import Rejestracja from "./pages/Rejestracja";
@@ -20,9 +17,6 @@ function App() {
       <main className="container page-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/o-nas/" element={<Onas />} />
-          <Route path="/uslugi/" element={<Uslugi />} />
-          <Route path="/kontakt/" element={<Kontakt />} />
           <Route path="/logowanie/" element={<Logowanie />} />
           <Route path="/rejestracja/" element={<Rejestracja />} />
           <Route path="/dashboard/" element={<Dashboard />} />
