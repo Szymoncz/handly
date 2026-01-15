660e5f6562acde10f44695e1c90687305857c4ac
Szymon <sz.czudowski@gmail.com>
2026-01-15 03:26:26 +0100

    Test 3

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,11 @@
 import Offer from "../components/Offer";
+import Footer from "../components/Footer";
 
 export default function Dashboard() {
   return (
     <>
       <Offer showAdd={true} showDelete={true} currentUserId={1} />
+      <Footer />
     </>
   );
 }
